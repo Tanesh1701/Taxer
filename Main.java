f2dec1986795bff6c7b5c641d2e0f6ac972f1a23
Tanesh1701 <tanesh1701@gmail.com>
2022-06-07 19:37:06 +0400

    Missed main

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+import screens.Register;
+
+public class Main extends JFrame{
+    public static void main(String[] args) {
+        Main main = new Main();
+        Register register = new Register();
+        main.add(register.getPanel());
+        main.setSize(750,600);
+        main.setVisible(true);
+    }
+
+    public Main() {
+        super("Taxer");
+    }
+}
