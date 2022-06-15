111fd2a1871d7bb2d1d60b11d54acafe6583cfb8
Tanesh1701 <tanesh1701@gmail.com>
2022-06-15 23:11:34 +0400

    Changed model class

--- models/TaxField.java
+++ models/TaxField.java
@@ -9,18 +9,27 @@ import javax.swing.*;
 public class TaxField {
     JPanel taxFieldPanel = new JPanel();
     JTextField taxTextField = new JTextField(20);
+    Constants constants = new Constants();
     
     public TaxField(String text) {
         taxFieldPanel.setLayout(new GridLayout(1,0));
         taxFieldPanel.setBackground(Color.WHITE);
 
-        JLabel textLabel = new JLabel(text);
+        // JLabel textLabel = new JLabel(text);
+        Text taxFieldLabel = new Text(text, 14);
 
         taxTextField.setBackground(Color.white);
         taxTextField.setMaximumSize(new Dimension(taxTextField.getPreferredSize()));
         taxTextField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.decode("#565656")));
 
-        taxFieldPanel.add(textLabel);
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, Text.class.getResourceAsStream("../Assets/Fonts/VarelaRound-Regular.TTF"));
+            taxTextField.setFont(font.deriveFont(Font.PLAIN, 14f));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        taxFieldPanel.add(taxFieldLabel.getTitle());
         taxFieldPanel.add(taxTextField);
     }
 
