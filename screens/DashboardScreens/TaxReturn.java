d00dfe2e56314c9a4fc89d76ff61cc95f6a5b8bc
Tanesh1701 <tanesh1701@gmail.com>
2022-06-24 19:19:22 +0400

    Made changes to tax return function

--- screens/DashboardScreens/TaxReturn.java
+++ screens/DashboardScreens/TaxReturn.java
@@ -1,11 +1,16 @@
 package screens.DashboardScreens;
+import java.awt.*;
+import models.Text;
+import java.io.*;
 import javax.swing.*;
 import models.Button;
 import models.TaxField;
-import models.Text;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.*;
+import java.util.Formatter;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.math.BigDecimal;
 
 public class TaxReturn extends JPanel{
 
@@ -20,21 +25,29 @@ public class TaxReturn extends JPanel{
     Text noDependentAmt = new Text("Rs 275, 000", 12);
     Text oneDependentAmt = new Text("Rs 385, 000", 12);
     Text taxRate;
-
+    Formatter outfile;
 
     JTextField textFieldRate = new JTextField(10);
 
-    JRadioButton radioButton1 = new JRadioButton("Mr."), 
-    radioButton2 = new JRadioButton("Mrs."), 
+    JRadioButton radioButton1 = new JRadioButton("Mr."), radioButton2 = new JRadioButton("Mrs."), 
     radioButton3 = new JRadioButton("Miss");
 
     JCheckBox checkBox1 = new JCheckBox(), checkBox2 = new JCheckBox();
 
     Button btnSubmit;
-        
 
     JSeparator separator = new JSeparator();
 
+    TaxField taxFieldName = new TaxField("Full name");
+    TaxField taxfieldAddress = new TaxField("Address");
+    TaxField taxFieldContact = new TaxField("Contact num");
+    TaxField taxFieldNIC = new TaxField("National Identity Card (NIC)");
+    TaxField taxFieldTAN = new TaxField("Tax Account Number (TAN)");
+    TaxField taxFieldTypeEmp = new TaxField("Type Of Employment");
+    TaxField taxFieldYIncome = new TaxField("Yearly Income");
+
+
+    double tax = 0, chargeableIncome = 0, dependent0 = 275000, dependent1 = 385000;
 
     public TaxReturn(){
         //radioBtnPanel and its components
@@ -44,13 +57,15 @@ public class TaxReturn extends JPanel{
         ButtonGroup bgCheck = new ButtonGroup();
         bgCheck.add(checkBox1);
         bgCheck.add(checkBox2);
+        CheckBoxHandler cbh = new CheckBoxHandler();
+        checkBox1.addItemListener(cbh);
+        checkBox2.addItemListener(cbh);
         bgRadio.add(radioButton1);
         bgRadio.add(radioButton2);
         bgRadio.add(radioButton3);
         radioBtnPanel.add(radioButton1);
         radioBtnPanel.add(radioButton2);
         radioBtnPanel.add(radioButton3);
-        radioButton1.setBackground(Color.WHITE);
         try {
             Font font = Font.createFont(Font.TRUETYPE_FONT, Text.class.getResourceAsStream("../Assets/Fonts/VarelaRound-Regular.TTF"));
             radioButton1.setFont(font.deriveFont(Font.PLAIN, 12f));
@@ -59,6 +74,7 @@ public class TaxReturn extends JPanel{
         } catch (Exception e) {
             System.out.println(e);
         }
+        radioButton1.setBackground(Color.WHITE);
         radioButton2.setBackground(Color.WHITE);
         radioButton3.setBackground(Color.WHITE);
         radioBtnPanel.setBackground(Color.WHITE);
@@ -84,27 +100,26 @@ public class TaxReturn extends JPanel{
         gbc1.gridy = 1; 
         fieldPanel.add(radioBtnPanel, gbc1);        
 
-        TaxField taxFieldName = new TaxField("Full Name");
         gbc1.gridx = 0;
         gbc1.gridy = 2;
         fieldPanel.add(taxFieldName.getTaxFieldPanel(), gbc1);
 
-        TaxField taxfieldAddress = new TaxField("Address");
+        
         gbc1.gridx = 0;
         gbc1.gridy = 3;
         fieldPanel.add(taxfieldAddress.getTaxFieldPanel(), gbc1);
 
-        TaxField taxFieldContact = new TaxField("Contact num");
+
         gbc1.gridx = 0;
         gbc1.gridy = 4;
         fieldPanel.add(taxFieldContact.getTaxFieldPanel(), gbc1);
 
-        TaxField taxFieldNIC = new TaxField("National Identity Card (NIC)");
+       
         gbc1.gridx = 0;
         gbc1.gridy = 5;
         fieldPanel.add(taxFieldNIC.getTaxFieldPanel(), gbc1);
 
-        TaxField taxFieldTAN = new TaxField("Tax Account Number (TAN)");
+
         gbc1.gridx = 0;
         gbc1.gridy = 6;
         fieldPanel.add(taxFieldTAN.getTaxFieldPanel(), gbc1);
@@ -121,12 +136,12 @@ public class TaxReturn extends JPanel{
         gbc1.gridy = 9; 
         fieldPanel.add(employeeLabel.getTitle(), gbc1);
 
-        TaxField taxFieldTypeEmp = new TaxField("Type Of Employment");
+
         gbc1.gridx = 0;
         gbc1.gridy = 10;
         fieldPanel.add(taxFieldTypeEmp.getTaxFieldPanel(), gbc1);
 
-        TaxField taxFieldYIncome = new TaxField("Yearly Income");
+        
         gbc1.gridx = 0;
         gbc1.gridy = 11;
         fieldPanel.add(taxFieldYIncome.getTaxFieldPanel(), gbc1);
@@ -167,6 +182,9 @@ public class TaxReturn extends JPanel{
         gbc1.gridy = 16; 
         fieldPanel.add(checkBox2, gbc1);
 
+        CheckBoxHandler handlerCheckBox = new CheckBoxHandler();
+        checkBox1.addItemListener(handlerCheckBox);
+        checkBox2.addItemListener(handlerCheckBox);
         
         checkBox1.setBackground(Color.WHITE);
         checkBox2.setBackground(Color.WHITE);
@@ -191,6 +209,7 @@ public class TaxReturn extends JPanel{
 
         btnSubmit = new Button("Submit");
         ButtonHandler handler = new ButtonHandler();
+        
         btnSubmit.getButton().addActionListener(handler);
         btnSubmit.getButton().setAlignmentX(Component.CENTER_ALIGNMENT);
 
@@ -206,10 +225,77 @@ public class TaxReturn extends JPanel{
         frame.setVisible(true);
     }
 
+    private class CheckBoxHandler implements ItemListener{
+
+        public void itemStateChanged(ItemEvent e){
+            
+            if(checkBox1.isSelected()){
+                chargeableIncome = Double.parseDouble(taxFieldYIncome.getTaxTextField().getText()) - dependent0;  
+                tax = (0.1/12) * chargeableIncome;
+                textFieldRate.setText(new BigDecimal(chargeableIncome).toPlainString());   
+            } else{
+                chargeableIncome = Double.parseDouble(taxFieldYIncome.getTaxTextField().getText()) - dependent1;
+                tax = (0.1/12) * chargeableIncome;
+                textFieldRate.setText(new BigDecimal(chargeableIncome).toPlainString());
+            }
+        }
+    }
+
     private class ButtonHandler implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent e){
+
+            String fullname = taxFieldName.getTaxTextField().getText();
+            String address = taxfieldAddress.getTaxTextField().getText();
+            String num = taxFieldContact.getTaxTextField().getText();
+            String nic = taxFieldNIC.getTaxTextField().getText();
+            String tan = taxFieldTAN.getTaxTextField().getText();
+            String employment = taxFieldTypeEmp.getTaxTextField().getText();
+            String income = taxFieldYIncome.getTaxTextField().getText();
+            Text errorMsg = new Text("You cannot leave any empty fields!", 14);
+
+            if(fullname.isEmpty() || address.isEmpty() || num.isEmpty() || nic.isEmpty() || tan.isEmpty() || employment.isEmpty() || income.isEmpty()){
+
+                JOptionPane.showMessageDialog(null, errorMsg.getTitle(), "Error", JOptionPane.ERROR_MESSAGE);
             
+            } else {
+                
+                if(!(fullname.matches(".*\\d.*") && employment.matches(".*\\d.*"))){
+
+                    if(num.matches("[0-9]+") && income.matches("[0-9]+")){
+
+                        if(nic.matches("[0-9]+") && nic.length() == 14){
+                            
+                            if(tan.matches("[0-9]+") && tan.length() == 8){
+                                
+                                try {  
+                                    Formatter outfile = new Formatter(new File("./screens/taxReturn.txt"));
+                                    outfile.format("%s %s %s %s %s %s %s %s %n", fullname, address, num, nic, tan, employment, income, tax);
+                                    
+                                    errorMsg = new Text("You're good to go bud!", 14);
+                                    JOptionPane.showMessageDialog(null, errorMsg.getTitle(), "Success", JOptionPane.INFORMATION_MESSAGE);                      
+                                    outfile.close();
+                                } catch (Exception exception) {
+                                    System.out.println(exception);
+                                }
+                            }else {
+                                errorMsg = new Text("Your TAN cannot be less than or greater than 8 digits!", 14);
+                                JOptionPane.showMessageDialog(null, errorMsg.getTitle(), "Error", JOptionPane.ERROR_MESSAGE);
+                            }
+                        } else {
+                            errorMsg = new Text("Your NIC cannot be less than or greater than 14 digits!", 14);
+                            JOptionPane.showMessageDialog(null, errorMsg.getTitle(), "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    } else {
+                        errorMsg = new Text("Your contact number cannot contain any alphabets or special characters!", 14);
+                        JOptionPane.showMessageDialog(null, errorMsg.getTitle(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } else {
+                    errorMsg = new Text("Your name cannot contain any numbers or special characters!", 14);
+                    JOptionPane.showMessageDialog(null, errorMsg.getTitle(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
         }
     }
 
@@ -219,6 +305,15 @@ public class TaxReturn extends JPanel{
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        TaxReturn tr = new TaxReturn();
+
+        f.add(tr.getMainPanel());
+        f.setSize(new Dimension(600,600));
+        f.setVisible(true);
     }
 
 }
