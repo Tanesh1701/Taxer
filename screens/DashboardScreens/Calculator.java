6ed7df0b5cf2f3334b59efd42f620330991baadd
Tanesh1701 <tanesh1701@gmail.com>
2022-06-25 19:56:12 +0400

    Closes #5

--- screens/DashboardScreens/Calculator.java
+++ screens/DashboardScreens/Calculator.java
@@ -1,7 +1,8 @@
 package screens.DashboardScreens;
 import models.*;
 import models.Button;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.Color;
@@ -12,11 +13,14 @@ public class Calculator extends JPanel{
     private GridLayout numberKeysGrid;
     private JTextField calculationArea;
     private JButton numberKey;
+    String s0, s1, s2;
 
     public Calculator(){
-
+        s0 = s1 = s2 = "";
         numberKeysGrid = new GridLayout( 4, 4, 0, 0);
         calculationArea = new JTextField(30);
+        calculationArea.setHorizontalAlignment(JTextField.RIGHT);
+        calculationArea.setEditable(false);
         numberKeysPanel = new JPanel();
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -25,7 +29,7 @@ public class Calculator extends JPanel{
 
         Text title = new Text("Calculator", 20);
         panel.add(title.getTitle(), BorderLayout.NORTH);
-        title.getTitle().setBorder(BorderFactory.createEmptyBorder(50,0,50,10));
+        title.getTitle().setBorder(BorderFactory.createEmptyBorder(30,0,50,10));
 
         try {
             Font font = Font.createFont(Font.TRUETYPE_FONT, Text.class.getResourceAsStream("../Assets/Fonts/VarelaRound-Regular.TTF"));
@@ -35,12 +39,14 @@ public class Calculator extends JPanel{
         }
 
         String[] labels = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"};
+        ButtonHandler bh = new ButtonHandler();
 
         for(int counter = 0; counter < labels.length; counter++){
-            JButton numberKey = new JButton(labels[counter]);
+            numberKey = new JButton(labels[counter]);
             numberKey.setFocusable(false);
             numberKey.setBackground(new Constants().getPrimaryColor());
             numberKey.setForeground(new Constants().getSecondaryColor());
+            numberKey.addActionListener(bh);
             numberKeysPanel.add(numberKey);
         }
         numberKeysPanel.setLayout(numberKeysGrid);
@@ -53,16 +59,87 @@ public class Calculator extends JPanel{
         panel.setVisible(true);
     }
 
+    private class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            String s = e.getActionCommand();
+
+            if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
+                // if operand is present then add to second no
+                if (!s1.equals(""))
+                    s2 = s2 + s;
+                else
+                    s0 = s0 + s;
+     
+                // set the value of text
+                calculationArea.setText(s0 + s1 + s2);
+            }
+            else if (s.charAt(0) == '=') {
+     
+                double te;
+     
+                // store the value in 1st
+                if (s1.equals("+"))
+                    te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+                else if (s1.equals("-"))
+                    te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+                else if (s1.equals("/"))
+                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+                else
+                    te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+     
+                // set the value of text
+                calculationArea.setText(s0 + s1 + s2 + "=" + te);
+     
+                // convert it to string
+                s0 = Double.toString(te);
+     
+                s1 = s2 = "";
+            }
+            else {
+                // if there was no operand
+                if (s1.equals("") || s2.equals(""))
+                    s1 = s;
+                // else evaluate
+                else {
+                    double te;
+     
+                    // store the value in 1st
+                    if (s1.equals("+"))
+                        te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+                    else if (s1.equals("-"))
+                        te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+                    else if (s1.equals("/"))
+                        te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+                    else
+                        te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+     
+                    // convert it to string
+                    s0 = Double.toString(te);
+     
+                    // place the operator
+                    s1 = s;
+     
+                    // make the operand blank
+                    s2 = "";
+                }
+     
+                // set the value of text
+                calculationArea.setText(s0 + s1 + s2);
+            }
+        }
+    }
+
     public JPanel getPanel() {
         return panel;
     }
 
-    // public static void main(String[] args) {
-    //     JFrame f = new JFrame();
-    //     Calculator tr = new Calculator();
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        Calculator tr = new Calculator();
 
-    //     f.add(tr.getPanel());
-    //     f.setSize(new Dimension(600,600));
-    //     f.setVisible(true);
-    // }
+        f.add(tr.getPanel());
+        f.setSize(new Dimension(600,600));
+        f.setVisible(true);
+    }
 }
