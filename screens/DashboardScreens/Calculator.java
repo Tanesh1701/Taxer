8dae6394ad4828d953ffba4ec75642df5ccb43b6
Tanesh1701 <tanesh1701@gmail.com>
2022-06-26 21:35:23 +0400

    Fixed calculator

--- screens/DashboardScreens/Calculator.java
+++ screens/DashboardScreens/Calculator.java
@@ -1,27 +1,24 @@
 package screens.DashboardScreens;
 import models.*;
-import models.Button;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.*;
+import models.Button;
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator extends JPanel{
 
-    private JPanel numberKeysPanel, panel;
+    private JPanel numberKeysPanel, panel, moreKeysPanel;
     private GridLayout numberKeysGrid;
     private JTextField calculationArea;
     private JButton numberKey;
+    Button del, clear;
     String s0, s1, s2;
 
     public Calculator(){
         s0 = s1 = s2 = "";
-        numberKeysGrid = new GridLayout( 4, 4, 0, 0);
-        calculationArea = new JTextField(30);
-        calculationArea.setHorizontalAlignment(JTextField.RIGHT);
-        calculationArea.setEditable(false);
-        numberKeysPanel = new JPanel();
+
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBackground(Color.WHITE);
@@ -31,12 +28,19 @@ public class Calculator extends JPanel{
         panel.add(title.getTitle(), BorderLayout.NORTH);
         title.getTitle().setBorder(BorderFactory.createEmptyBorder(30,0,50,10));
 
+        calculationArea = new JTextField(30);
+        calculationArea.setHorizontalAlignment(JTextField.RIGHT);
+        calculationArea.setEditable(false);
+
         try {
             Font font = Font.createFont(Font.TRUETYPE_FONT, Text.class.getResourceAsStream("../Assets/Fonts/VarelaRound-Regular.TTF"));
             calculationArea.setFont(font.deriveFont(Font.PLAIN, 24f));
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        numberKeysGrid = new GridLayout( 4, 4, 0, 0);
+        numberKeysPanel = new JPanel();
 
         String[] labels = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"};
         ButtonHandler bh = new ButtonHandler();
@@ -53,9 +57,22 @@ public class Calculator extends JPanel{
         numberKeysPanel.setPreferredSize(new Dimension(600, 500));
         numberKeysPanel.setBorder(BorderFactory.createEmptyBorder(-1,0,-2,0));
 
+        moreKeysPanel = new JPanel();
+        moreKeysPanel.setBackground(Color.WHITE);
+        moreKeysPanel.setLayout(new FlowLayout());
+        moreKeysPanel.setPreferredSize(new Dimension(500, 75));
+        moreKeysPanel.setBorder(BorderFactory.createEmptyBorder(0,0,5,0));
+
+        clear = new Button("Clear");
+        clear.getButton().addActionListener(bh);
+        clear.getButton().setPreferredSize(new Dimension(100, 50));
+        clear.getButton().setCursor(new Cursor(Cursor.HAND_CURSOR));
+        moreKeysPanel.add(clear.getButton());
+
         calculationArea.setPreferredSize(new Dimension(600, 60));
         panel.add(calculationArea, BorderLayout.CENTER);
         panel.add(numberKeysPanel, BorderLayout.SOUTH);
+        panel.add(moreKeysPanel);
         panel.setVisible(true);
     }
 
@@ -70,6 +87,12 @@ public class Calculator extends JPanel{
                     s2 = s2 + s;
                 else
                     s0 = s0 + s;
+     
+                // set the value of text
+                calculationArea.setText(s0 + s1 + s2);
+            } else if (s.equals("Clear")) {
+                // clear the one letter
+                s0 = s1 = s2 = "";
      
                 // set the value of text
                 calculationArea.setText(s0 + s1 + s2);
